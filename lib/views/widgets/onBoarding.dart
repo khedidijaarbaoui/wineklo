1b4ddc119e9a5df99abe76c146e014aef03efe82
Razane Lagagna <146004835+RazaneLagg@users.noreply.github.com>
2024-11-29 14:47:45 +0100

    Update onBoarding.dart

--- lib/views/widgets/onBoarding.dart
+++ lib/views/widgets/onBoarding.dart
@@ -14,23 +14,31 @@ class OnboardingWidget extends StatelessWidget {
       mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.center,
       children: [
-        Image.asset(
-          item.imagePath,
-          width: 300,
-          height: 200,
-          fit: BoxFit.cover,
+        ClipRRect(
+          borderRadius: BorderRadius.circular(12),
+          child: Image.asset(
+            item.imagePath,
+            width: 240,
+            height: 292,
+            fit: BoxFit.cover,
+          ),
         ),
         const SizedBox(height: 20),
+        // Title
         Text(
           item.title,
           style: headlineStyle.copyWith(color: blackColor),
           textAlign: TextAlign.center,
         ),
-        const SizedBox(height: 10),
-        Text(
-          item.subtitle,
-          style: bodyTextStyle.copyWith(color: darkGrayColor),
-          textAlign: TextAlign.center,
+        const SizedBox(height: 15),
+        // Subtitle with consistent margin
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0),
+          child: Text(
+            item.subtitle,
+            style: bodyTextStyle.copyWith(color: darkGrayColor),
+            textAlign: TextAlign.center,
+          ),
         ),
       ],
     );
