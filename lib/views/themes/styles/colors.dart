9897f2e05d16e507bbaebe8e543f46921349aec2
Razane Lagagna <146004835+RazaneLagg@users.noreply.github.com>
2024-11-29 14:53:46 +0100

    Update colors.dart

--- lib/views/themes/styles/colors.dart
+++ lib/views/themes/styles/colors.dart
@@ -6,9 +6,9 @@ const darkOrangeColor = Color.fromRGBO(251, 109, 58,1);
 
 const textLightGrayColor = Color.fromRGBO(241, 241, 241, 1);// for text
 const lightGrayColor = Color.fromRGBO(212, 217, 223, 1);// for placeholders
-
 const darkGrayColor = Color.fromRGBO(137, 138, 141, 1);//for text
 
+const darkBlueColor = Color.fromRGBO(18, 18, 35, 1);
 
 const blackColor = Color.fromRGBO(0, 0, 0, 1);
 const whiteColor = Color.fromRGBO(255, 255, 255, 1);
